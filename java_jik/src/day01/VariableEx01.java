a71afdcaff4fd3ba54a588bfc0b2d3c9f72ba6e9
lana9722 <congo0206@naver.com>
2024-05-28 17:11:08 +0900

    자바 1일차 : 변수 - 변수 선언 예제

---
+++ java_jik/src/day01/VariableEx01.java
@@ -0,0 +1,24 @@
+package day01;
+
+public class VariableEx01 {
+
+	public static void main(String[] args) {
+		
+		char ch1;//문자형 변수 ch1를 선언
+		char ch2 = 'A';//문자형 변수 ch2를 선언하고 A로 초기화
+		//문자형 변수 ch3과 ch4를 선언
+		/*
+		char ch3;
+		char ch4;
+		*/
+		char ch3, ch4;//같은 자료형인 경우 , 를 통해 동시에 선언할 수 있다
+		//문자형 변수 ch5를 선언하고 +로 초기화
+		//문자형 변수 ch6을 선언하고 \n으로 초기화
+		char ch5 = '+', ch6 = '\n';
+		System.out.println(ch2);
+		System.out.println(ch6);
+		System.out.println(ch5);
+		
+	}
+
+}
