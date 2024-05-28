359714be4bcc0eb865ff0443e39dc229dbfcae50
lana9722 <congo0206@naver.com>
2024-05-29 08:59:02 +0900

    ??

--- java_jik/src/day01/VariableEx01.java
+++ java_jik/src/day01/VariableEx01.java
@@ -19,6 +19,12 @@ public class VariableEx01 {
 		System.out.println(ch6);
 		System.out.println(ch5);
 		
+		long num1 = 1234567890123L;//정수값이 int형으로 표현되지 않으면 접미사를 반드시 붙여야 함
+		//long은 접미사 L 또는 l을 사용(필수는 아님. 큰 숫자에는 필수)
+		//정수는 리터럴은 기본이 int고 실수 리터럴은 기본이 double
+		float num2 = 3.14F; // 3.14는 double형이기 때문에 반드시 접미사를 붙여야함
+		double num3 = 3.14; // double은 실수 기본이니까 접미를 안붙혀도 됌
+		boolean isEven = true;
 	}
 
 }
