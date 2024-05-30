9373c508570da0dfd5c2f1f7d823e70c967f5070
lana9722 <congo0206@naver.com>
2024-05-30 17:35:21 +0900

    Update SwitchExam02.java

--- java_jik/src/day03/SwitchExam02.java
+++ java_jik/src/day03/SwitchExam02.java
@@ -2,37 +2,41 @@ package day03;
 
 import java.util.Scanner;
 
-public class SwitchExam02{
+public class SwitchExam02 {
 
 	public static void main(String[] args) {
-	/*월을 받아 입력받은 월의 계절을 출력하세요.(switch문을 이용하여)
-	 * 345
-	 * 678
-	 * 91011
-	 * 1212 겨울
-	 * 그외 잘못된월
-	 */
-		
+		/* 월을 입력받아 입력받은 월의 계절을 출력하세요.(switch문 이용하여)
+		 * 3,4,5 : 봄
+		 * 6,7,8 : 여름
+		 * 9,10,11 : 가을
+		 * 12, 1, 2 : 겨울
+		 * 그 외 : 잘못된 월
+		 * */
 		Scanner scan = new Scanner(System.in);
-		System.out.println("정수를 입력하세요 : ");
-		int num = scan.nextInt();
-		
-		
-		switch(num) {
-		case 3,4,5: 
-			System.out.println("월은 봄입니다");
+		System.out.print("월 입력 : ");
+		int month = scan.nextInt();
+
+		switch(month) {
+		case 3: 
+		case 4: 
+		case 5:
+		//case 3,4,5:
+			System.out.println("봄");
 			break;
-		case 6,7,8: 
-			System.out.println("월은 여름입니다");
+		case 6,7,8:
+			System.out.println("여름");
 			break;
-		case 9,10,11: 
-			System.out.println( "월은 가을입니다");
+		case 9,10,11:
+			System.out.println("가을");
 			break;
-		case 12,1,2: 
-			System.out.println( "월은 겨울입니다");
+		case 12,1,2:
+			System.out.println("겨울");
 			break;
-		default : 
-			System.out.println( "잘못된 월입니다.");
-			
+		default:
+			System.out.println("잘못된 월");
 		}
-	
+
+
+	}
+
+}
